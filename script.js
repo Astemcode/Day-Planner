ddc2370c4eae496664633b2eb0ecc4c2c1fcaa5d
Joivre Charles <astemcode@gmail.com>
2020-11-01 16:11:30 -0500

    Update script.js
    All text is now saved and task will be shown for entire day

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var eightAm = $("#8am");
-
-$(document).ready(function() {    
+$(document).ready(function() {
 	// Time Set
 	var currentDate = moment().format('dddd') + ' ' + moment().format('Do MMM YYYY');
 	var timeInterval = setInterval(function() {
@@ -11,53 +9,58 @@ $(document).ready(function() {
 		$('#currentDay').html(currentDate + ' ' + currentTime.format('hh:mm:ss A'));
 	}, 10);
 
+	// Time Block Color change Using Loops
+	function blockTimes() {
+		var presentTime = moment().hour();
 
-    // Time Block Color change Using Loops
-    function blockTimes() {
-        var presentTime = moment().hour();
+		$('.time-block').each(function() {
+			var hourSection = parseInt($(this).attr('id').split('hour')[1]);
+			console.log(hourSection, presentTime);
 
-        $(".time-block").each(function () {
-            var hourSection = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( hourSection, presentTime)
+			if (hourSection < presentTime) {
+				$(this).addClass('past');
+				$(this).removeClass('future');
+				$(this).removeClass('present');
+			} else if (hourSection === presentTime) {
+				$(this).removeClass('past');
+				$(this).addClass('present');
+				$(this).removeClass('future');
+			} else {
+				$(this).removeClass('present');
+				$(this).removeClass('past');
+				$(this).addClass('future');
+			}
+		});
+	}
 
-            if (hourSection < presentTime) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            else if (hourSection === presentTime) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
+	// Save Button for Daily Task
+	$('.saveBtn').on('click', function() {
+		console.log(this);
+		var text = $(this).siblings('.description').val();
+		var time = $(this).parent().attr('id');
 
-    // Storage Function for saving Task 
-    function taskLog() {
-        var task8 = JSON.parse(localStorage.getItem("8:00am"));
-        eightAm.val(task8);
-    }
-    $(".saveBtn").on("click", function() {
-        console.log(this)
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+		//Items saved in localStorage
+		localStorage.setItem(time, text);
+	});
 
-        //set items in local storage.
-        localStorage.setItem(time, text);
-    })
+	// Storage of Task using ID and Class
+	$('#hour8 .description').val(localStorage.getItem('hour8'));
+	$('#hour9 .description').val(localStorage.getItem('hour9'));
+	$('#hour10 .description').val(localStorage.getItem('hour10'));
+	$('#hour11 .description').val(localStorage.getItem('hour11'));
+	$('#hour12 .description').val(localStorage.getItem('hour12'));
+	$('#hour13 .description').val(localStorage.getItem('hour13'));
+	$('#hour14 .description').val(localStorage.getItem('hour14'));
+	$('#hour15 .description').val(localStorage.getItem('hour15'));
+	$('#hour16 .description').val(localStorage.getItem('hour16'));
+	$('#hour17 .description').val(localStorage.getItem('hour17'));
+	$('#hour18 .description').val(localStorage.getItem('hour18'));
 
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-
-
-    blockTimes();
+	blockTimes();
     taskLog();
-
-    })
-  
-
+    
+    $("#clearDay").on("click", function(){
+        localStorage.clear();
+        initPage()
+      }) 
+});
